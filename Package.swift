3863c49128fa4dfc9464d5d1eee8096fa5fa177f
Facephi <developer@facephi.com>
2026-07-02 10:58:22 +0000

    New version 6.2.3

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/facephi-clienters/FPHIRecognitionHelper-SPM.git", .upToNextMajor(from: "3.27.2")),
-        .package(url: "https://github.com/facephi-clienters/FPHISelphiResources-SPM.git", .exactItem("6.2.1")),
+        .package(url: "https://github.com/facephi-clienters/FPHISelphiResources-SPM.git", .exactItem("6.2.3")),
         .package(url: "https://github.com/facephi-clienters/FPBTokenizer-SPM.git", .exactItem("4.0.4")),
         .package(url: "https://github.com/facephi-clienters/FPHILicenseManager-SPM.git", .upToNextMajor(from: "0.5.7")),
         .package(url: "https://github.com/facephi-clienters/FPHIIADFrameworks-SPM.git", .upToNextMajor(from: "2.5.1"))
@@ -35,11 +35,11 @@ let package = Package(
 
                     
         .binaryTarget(name: "FPhiWidgetCore",
-        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/Selphi/FPHISelphiIADFrameworks/6.2.1/FPhiWidgetCore.zip",
-        checksum: "2c885bbb06f787179962f428da432f3fc7fb70af310526d183e821f43e7e16af"),
+        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/Selphi/FPHISelphiIADFrameworks/6.2.3/FPhiWidgetCore.zip",
+        checksum: "6a650e27710736a7c49cc5e8833a7c577a0421fe9a588762dea97575df8c23b6"),
         .binaryTarget(name: "FPhiWidgetSelphi",
-        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/Selphi/FPHISelphiIADFrameworks/6.2.1/FPhiWidgetSelphi.zip",
-        checksum: "1028985f8f4668a7c6e02c705253495c550258315e2993cbedef9b33bc6389e9")
+        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/Selphi/FPHISelphiIADFrameworks/6.2.3/FPhiWidgetSelphi.zip",
+        checksum: "b1e49fdcc9d84d444319faeae049fab3692c0b110817c1b8a40160748a56d58f")
         
     ]
 )
